4c522c0c6f2986dc24b72a25710d6c9ada07685e
ajaykonda981 <ajaykonda981@gmail.com>
2024-09-06 07:30:46 +0530

    post method

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   {
+        path: 'department',
+        component: DepartmentComponent
+   }
+
+];
